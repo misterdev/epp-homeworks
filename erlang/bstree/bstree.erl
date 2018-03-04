52cdaf39c6460bbf41ba7bfb73d1478419dbaf69
Devid Farinelli <devid.farinelli@gmail.com>
2018-03-04 05:34:22 +0100

    Remove key from bstree

--- erlang/bstree/bstree.erl
+++ erlang/bstree/bstree.erl
@@ -4,8 +4,8 @@
 empty() -> nil .
 
 insert(K, nil) -> {node, K, nil, nil} ; 
-insert(K, {node, K1, L, R}) when K < K1-> {node, K1, insert(K, L), R} ;
-insert(K, {node, K1, L, R}) when K > K1-> {node, K1, L, insert(K, R)} ;
+insert(K, {node, K1, L, R}) when K < K1 -> {node, K1, insert(K, L), R} ;
+insert(K, {node, K1, L, R}) when K > K1 -> {node, K1, L, insert(K, R)} ;
 insert(_K, T) -> T .
 
 foldl(_F, R, []) -> R ;
@@ -16,18 +16,27 @@ mappend(T1, nil) ->  T1 ;
 mappend(nil, T2) -> T2 ;
 mappend({node, K, L, R}, T2) -> insert(K, mappend(L, mappend(R, T2))).
 
-foldtree(F, RET, nil) -> RET ;
+foldtree(_F, RET, nil) -> RET ;
+foldtree(_F, nil, T) -> T ;
 foldtree(F, RET, {node, K, L, R}) -> foldtree(F, foldtree(F, F(K, RET), L), R) .
+
+% REMOVE
+remove(_K, nil) -> nil ;
+remove(K, {node, K1, L, R}) when K == K1 -> mappend(R, L);
+remove(K, {node, K1, L, R}) -> {node, K1, remove(K, L), remove(K, R)} .
 
 main() ->
 	Tree = foldl(fun insert/2, empty(), [8, 3, 10, 14]),
 	Tree2 = foldl(fun insert/2, empty(), [ 6, 7, 13, 4, 1, 3]),
 	% mappend(Tree2, Tree).
-	foldtree(fun insert/2, Tree, Tree2) .
+	Tree3 = foldtree(fun insert/2, Tree, Tree2) ,
+	remove(8, Tree3) .
+
+
 
 
 % Esercizi
 % 1. Scrivere la funzione che rimuove una chiave da un BST
 % 2. Scrivere la funzione append:
-% 		- a mano
+% 		- a mano 
 % 		- usando una fold
