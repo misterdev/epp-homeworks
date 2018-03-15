7b7371939fc0c352a28eda2108ec443df652fe2f
Devid Farinelli <devid.farinelli@gmail.com>
2018-03-15 13:09:24 +0100

    Refactored bstree es 1,2

--- erlang/bstree/bstree.erl
+++ erlang/bstree/bstree.erl
@@ -1,6 +1,8 @@
 % Homeworks
-% 1. Create a function that removes a key from a BST
-% 2. Create the append function: naïve and using a fold
+% 1. Scrivere la funzione che rimuove una chiave da un BST
+% 2. Scrivere la funzione append senza usare una fold e usando una fold.
+   % La funzione implemenata è tail-ricorsiva? Altrimenti fornire un'altra
+   % implementazione tail ricorsiva.
 
 - module(bstree) .
 - export([main/0]) .
@@ -15,24 +17,25 @@ insert(_K, T) -> T .
 foldl(_F, R, []) -> R ;
 foldl(F, R, [H | TL]) -> foldl(F, F(H, R), TL) .
 
-% APPEND
-mappend(T1, nil) ->  T1 ;
-mappend(nil, T2) -> T2 ;
-mappend({node, K, L, R}, T2) -> insert(K, mappend(L, mappend(R, T2))).
-
-foldtree(_F, nil, RET) -> RET ;
-foldtree(_F, T, nil) -> T ;
-foldtree(F, {node, K, L, R}, RET) -> foldtree(F, R, foldtree(F, L, F(K, RET))) .
-
-% REMOVE
+% 1. REMOVE
 remove(_K, nil) -> nil ;
 remove(K, {node, K1, L, R}) when K < K1 -> {node, K1, remove(K, L), R} ;
 remove(K, {node, K1, L, R}) when K > K1 -> {node, K1, L, remove(K, R)} ;
-remove(_, {node, _, L, R}) -> mappend(R, L) .
+remove(_, {node, _, L, R}) -> append(R, L) .
+
+% 2. APPEND
+append(T1, nil) ->  T1 ;
+append(nil, T2) -> T2 ;
+append({node, K, L, R}, T2) -> insert(K, append(L, append(R, T2))).
+
+foldappend(_F, nil, RET) -> RET ;
+foldappend(_F, T, nil) -> T ;
+foldappend(F, {node, K, L, R}, RET) -> foldappend(F, R, foldappend(F, L, F(K, RET))) .
+
 
 main() ->
 	Tree = foldl(fun insert/2, empty(), [8, 3, 10, 14]),
 	Tree2 = foldl(fun insert/2, empty(), [ 6, 7, 13, 4, 1, 3]),
-	% mappend(Tree2, Tree).
-	Tree3 = foldtree(fun insert/2, Tree, Tree2) ,
+	% append(Tree2, Tree).
+	Tree3 = foldappend(fun insert/2, Tree, Tree2) ,
 	remove(3, Tree3) .
