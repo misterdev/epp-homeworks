6b531ca3a9fee2b7e49da5ec184298623a9c9fa0
Devid Farinelli <devid.farinelli@gmail.com>
2018-03-30 15:10:32 +0200

    Done smoker

--- erlang/cigasp.erl
+++ erlang/cigasp.erl
@@ -6,41 +6,64 @@
 
 % Every Smoker has an infinite supply of one of the three ingredients 
 % Arbiter randomly selects two of the supplies to place on the table
-% The third Smoker takes the supplies and smoker for a while
+% The third Smoker takes the supplies and smokes for a while
 
 - module(cigasp) .
-- export([main/0, table/0, smoker/4]) .
+- export([main/0, table/0, smoker/3, arbiter/3]) .
 
-- define(nsmokers, 3) .
-- define(iterations, 10) .
+- define(ingredients, 3) .
+- define(iterations, 2) .
 
 % UTILS
 sleep(N) -> receive after N * 100 -> ok end .
 
 % AGENTS
 
-table() -> ok.
+table() -> 
+    receive
+        X -> io:format("RECEIVED ~p ~n", [X]),
+        table()
+    end
+.
 
-arbiter() -> 
-    sleep(rand:uniform(3)) .
+arbiter(Main, _, 0) -> Main ! exit ;
 
-get_materials(Table, SmokerId) ->
-	Table ! {get, self(), lists:seq(0, ?nsmokers) --  [SmokerId]} ,
-	receive ok -> ok end .
-
-smoker(Main, Table, SmokerId, Iteration) when Iteration > 0 ->
-    io:format("Smoker ~p waits materials~n", [SmokerId]) ,
-    get_materials(Table, SmokerId) ,
-    io:format("Smoker ~p is smoking~n", [SmokerId]) ,
+arbiter(Main, Smokers, Iteration) ->
+    IgnoredSupply = rand:uniform( ?ingredients ) - 1 ,
+    [ Smoker ! { place, lists:seq(0, ?ingredients - 1) -- [IgnoredSupply] } || Smoker <- Smokers ] ,
     sleep(rand:uniform(5)) ,
-    smoker(Main, Table, SmokerId, Iteration - 1) ;
-    
-smoker(Main, _, _, 0) ->
-    Main ! exit .
+    arbiter(Main, Smokers, Iteration - 1 )
+.
+
+
+get_materials(Table, Supplies) ->
+	Table ! { get, self(), Supplies }
+	% receive ok -> ok end
+.
+
+smoker(Main, Table, SmokerId) ->
+    receive
+        { place, [Supply1, Supply2] } when Supply1 == SmokerId orelse Supply2 == SmokerId ->
+            % io:format("~p JLKJLKJL, ~p ~p ~n", [SmokerId, Supply1, Supply2]) ,
+            Table ! { put, SmokerId } ;
+        { place, Supplies } ->
+            % io:format("~p JLKJLKJL, ~p ~n", [SmokerId, Supplies]) ,
+            get_materials(Table, Supplies) ,
+            sleep(rand:uniform(5)) % Smokes for some time
+        end ,
+        smoker(Main, Table, SmokerId)
+.
+    % io:format("Smoker ~p waits materials~n", [SmokerId]) ,
+    % get_materials(Table, SmokerId) ,
+    % io:format("Smoker ~p is smoking~n", [SmokerId]) ,
+    % sleep(rand:uniform(5)) ,
+    % smoker(Main, Table, SmokerId) .
 
 main() ->
-    Smokers = lists:seq(0, ?nsmokers) ,
     Table = spawn(?MODULE, table, []) ,
-    [ spawn(?MODULE, smoker, [self(), Table, SmokerId, iterations]) || SmokerId <- Smokers ] 
+    Smokers = [ spawn(?MODULE, smoker, [self(), Table, SmokerId]) || SmokerId <- lists:seq(0, ?ingredients - 1) ] ,
+    Arbiter = spawn(?MODULE, arbiter, [self(), Smokers, ?iterations]) 
+
+    % Tup
     % SPAWN AGENT WITH SMOKERS PIDS
 .
